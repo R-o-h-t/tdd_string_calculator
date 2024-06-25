bb2dd244d64e4976cfd78186aa041862ffdceea1
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2024-06-25 11:15:43 +0200

    feat: Add support for 1 or 2 numbers

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,3 +1,13 @@
 int add(String numbers) {
-  return 0;
+  if (numbers.isEmpty) {
+    return 0;
+  }
+  final numbersList = numbers.split(',');
+  if (numbersList.length == 1) {
+    return int.parse(numbersList[0]);
+  }
+  if (numbersList.length == 2) {
+    return int.parse(numbersList[0]) + int.parse(numbersList[1]);
+  }
+  throw UnimplementedError();
 }
