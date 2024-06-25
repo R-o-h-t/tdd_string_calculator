067b6b9af07f385556da821917bc24a4760087eb
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2024-06-25 11:55:20 +0200

    Refactor add function to handle specifying a delimiter

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,6 +2,17 @@ int add(String numbers) {
   if (numbers.isEmpty) {
     return 0;
   }
+
+  // handle specifying a delimiter
+  // "//[delimiter]\n[numbers]"
+  if (numbers.startsWith('//')) {
+    // take the delimiter (between the // and the \n)
+    final delimiter = numbers.substring(2, numbers.indexOf('\n'));
+    numbers = numbers.substring(numbers.indexOf('\n') + 1);
+    // replace the delimiter with a comma
+    numbers = numbers.replaceAll(delimiter, ',');
+  }
+
   // handle new lines as delimiters
   numbers = numbers.replaceAll('\n', ',');
   final numbersList = numbers.split(',');
