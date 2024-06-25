dab133b9a2d15390b73b191128590616d4f33f45
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2024-06-25 11:09:34 +0200

    step 1 : testing empty string

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,1 +1,3 @@
-
+int add(String numbers) {
+  throw new UnimplementedError();
+}
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,1 +1,11 @@
-void main() {}
+import 'package:string_calculator/string_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test(
+    'should return 0 for an empty string',
+    () {
+      expect(add(''), 0);
+    },
+  );
+}
