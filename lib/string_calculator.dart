1c0c11ccd237b7460a911072648721f9a116523f
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2024-06-25 11:20:26 +0200

    chore: Refactor add function to handle new lines as delimiters and throw error for invalid input

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,6 +2,13 @@ int add(String numbers) {
   if (numbers.isEmpty) {
     return 0;
   }
+  // handle new lines as delimiters
+  numbers = numbers.replaceAll('\n', ',');
   final numbersList = numbers.split(',');
+  // give error if multiple delimiters are found together
+  if (numbersList.contains('')) {
+    throw FormatException('Invalid input');
+  }
+
   return numbersList.map(int.parse).reduce((a, b) => a + b);
 }
