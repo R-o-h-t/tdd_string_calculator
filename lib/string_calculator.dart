ea2f286d5fee2b11ac1f2c883efd0b8e0bf84c17
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2024-06-25 11:56:00 +0200

    Refactor add function to handle specifying a delimiter

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,21 +1,32 @@
+String? _getDelimiter(String numbers) {
+  if (numbers.startsWith('//')) {
+    final delimiter = numbers.substring(2, numbers.indexOf('\n'));
+    return delimiter;
+  }
+  return null;
+}
+
 int add(String numbers) {
   if (numbers.isEmpty) {
     return 0;
   }
 
-  // handle specifying a delimiter
-  // "//[delimiter]\n[numbers]"
-  if (numbers.startsWith('//')) {
-    // take the delimiter (between the // and the \n)
-    final delimiter = numbers.substring(2, numbers.indexOf('\n'));
-    numbers = numbers.substring(numbers.indexOf('\n') + 1);
-    // replace the delimiter with a comma
-    numbers = numbers.replaceAll(delimiter, ',');
-  }
+  // if a delimiter is passed, only this delimiter should be used
+  final delimiter = _getDelimiter(numbers) ?? ',';
+  // remove the delimiter from the string
+  numbers = numbers.replaceAll(RegExp(r'//.*\n'), '');
 
   // handle new lines as delimiters
-  numbers = numbers.replaceAll('\n', ',');
-  final numbersList = numbers.split(',');
+  numbers = numbers.replaceAll('\n', delimiter);
+
+  // if a non number is found (different from the delimiter), throw an error indicating "[delimiter] expected but [found] found at position [position]"
+  if (numbers.contains(RegExp('[^0-9$delimiter]'))) {
+    final position = numbers.indexOf(RegExp('[^0-9$delimiter]'));
+    throw FormatException(
+        "'$delimiter' expected but '${numbers[position]}' found at position $position.");
+  }
+
+  final numbersList = numbers.split(delimiter);
   // give error if multiple delimiters are found together
   if (numbersList.contains('')) {
     throw FormatException('Invalid input');
