34c964e9245a38c61b5eb07e116633a0c1f45f2d
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2024-06-27 10:27:11 +0200

    refactor: Update add function to handle new lines between numbers

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,6 +3,8 @@ int add(String numbers) {
   if (numbers.isEmpty) {
     return 0;
   }
+  numbers = numbers.replaceAll('\n', ',');
+
   final List<int> numbersList = numbers.split(',').map(int.parse).toList();
   return numbersList.reduce((int a, int b) => a + b);
 }
