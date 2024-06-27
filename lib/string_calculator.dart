3d2bad7f4eb0c542fe2cb2780c307f2adab19b0b
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2024-06-27 10:42:37 +0200

    impl : error if ends with delimiter

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -11,6 +11,10 @@ int add(String numbers) {
   if (numbers.startsWith(',')) {
     throw FormatException('Invalid delimiter at position 0');
   }
+  if (numbers.endsWith(',')) {
+    throw FormatException(
+        'Invalid delimiter at position ${numbers.length - 1}');
+  }
 
   final List<int> numbersList = numbers.split(',').map(int.parse).toList();
   return numbersList.reduce((int a, int b) => a + b);
